b4c681375685749042a28a9a32804c81eb8af258
IMopCarpets <gharetmc@gmail.com>
2026-03-29 01:46:07 -0400

    Implement bookmark management functionality

---
+++ app.js
@@ -0,0 +1,45 @@
+let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+function save() {
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
+
+function render() {
+  const container = document.getElementById("bookmarks");
+  container.innerHTML = "";
+
+  bookmarks.forEach((b, i) => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <h3>${b.name}</h3>
+      <a href="${b.url}" target="_blank">Open</a>
+      <div class="delete" onclick="removeBookmark(${i})">Delete</div>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+function addBookmark() {
+  const name = document.getElementById("name").value;
+  const url = document.getElementById("url").value;
+
+  if (!name || !url) return;
+
+  bookmarks.push({ name, url });
+  save();
+  render();
+
+  document.getElementById("name").value = "";
+  document.getElementById("url").value = "";
+}
+
+function removeBookmark(index) {
+  bookmarks.splice(index, 1);
+  save();
+  render();
+}
+
+render();
